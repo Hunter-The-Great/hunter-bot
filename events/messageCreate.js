8740fbbd506bfd9a82a80f6be80df625ac0cead0
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2023-05-02 00:54:31 -0500

    last == changed

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,7 @@ const { Events } = require("discord.js");
 module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
-        if (message.content.toLowerCase() == "hello there") {
+        if (message.content.toLowerCase() === "hello there") {
             message.channel.send("General Kenobi");
         }
     },
