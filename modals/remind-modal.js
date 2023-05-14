ec91174a5abfc85b2e4a313ae0fd02ce35b92630
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2023-05-14 14:48:56 -0500

    more more testing

--- modals/remind-modal.js
+++ modals/remind-modal.js
@@ -1,7 +1,7 @@
 const fetch = require("isomorphic-fetch");
 
 const execute = async (interaction) => {
-    const delay = new Date.parse(interaction.fields.getTextInputValue("delay"));
+    const delay = new Date(interaction.fields.getTextInputValue("delay"));
     const reminder = interaction.fields.getTextInputValue("remindercontent");
     const response = await fetch(
         "https://qstash.upstash.io/v1/publish/https://hunter-bot-production.up.railway.app/reminders",
