671f128a54869c7c88c6d08732c91a3b0900c3c7
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2023-05-20 17:13:20 -0500

    removing moment stuff cause moment is bad

--- modals/remind-modal.js
+++ modals/remind-modal.js
@@ -1,17 +1,14 @@
 const fetch = require("isomorphic-fetch");
-var moment = require("moment-timezone");
 
 const execute = async (interaction) => {
     const delay = new Date(interaction.fields.getTextInputValue("delay"));
-
-    const test = moment.tz(interaction.fields.getTextInputValue("delay"), [
-        "MM/DD/YY hh:mm a ZZZ",
-        "MM/DD hh:mm a ZZZ",
-        "hh:mm a ZZZ",
-    ]);
-    console.log(test.unix());
+    if (delay.toString() === "Invalid Date") {
+        interaction.reply({
+            content: "Invalid date format, please follow the example.",
+        });
+        return;
+    }
     const reminder = interaction.fields.getTextInputValue("remindercontent");
-
     const response = await fetch(
         "https://qstash.upstash.io/v1/publish/https://hunter-bot-production.up.railway.app/reminders",
         {
@@ -19,7 +16,7 @@ const execute = async (interaction) => {
             headers: {
                 Authorization: "Bearer " + process.env.QSTASH_TOKEN,
                 "Content-type": "application/json",
-                "Upstash-Not-Before": test.unix(),
+                "Upstash-Not-Before": Math.floor(delay.getTime() / 1000),
             },
             body: JSON.stringify({
                 key: process.env.KEY,
@@ -28,7 +25,6 @@ const execute = async (interaction) => {
             }),
         }
     );
-
     if (!response.ok) {
         await interaction.reply({
             content: "Failed to set reminder, please try again later.",
