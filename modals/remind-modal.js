978335ca755b54932e660430a7abe700b6206227
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2023-05-20 16:30:25 -0500

    testing date parsing

--- modals/remind-modal.js
+++ modals/remind-modal.js
@@ -1,4 +1,5 @@
 const fetch = require("isomorphic-fetch");
+var moment = require("moment");
 
 const execute = async (interaction) => {
     const delay = new Date(interaction.fields.getTextInputValue("delay"));
@@ -8,7 +9,12 @@ const execute = async (interaction) => {
         });
         return;
     }
+    const test = moment(
+        interaction.fields.getTextInputValue("delay"),
+        "MM/DD/YY hh:mm a"
+    );
     const reminder = interaction.fields.getTextInputValue("remindercontent");
+
     const response = await fetch(
         "https://qstash.upstash.io/v1/publish/https://hunter-bot-production.up.railway.app/reminders",
         {
@@ -16,7 +22,7 @@ const execute = async (interaction) => {
             headers: {
                 Authorization: "Bearer " + process.env.QSTASH_TOKEN,
                 "Content-type": "application/json",
-                "Upstash-Not-Before": Math.floor(delay.getTime() / 1000),
+                "Upstash-Not-Before": test.unix(),
             },
             body: JSON.stringify({
                 key: process.env.KEY,
@@ -25,6 +31,7 @@ const execute = async (interaction) => {
             }),
         }
     );
+
     if (!response.ok) {
         await interaction.reply({
             content: "Failed to set reminder, please try again later.",
