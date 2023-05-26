e71b5f3db6bef7f11a964d6c935dca5ff79486da
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2023-05-25 21:45:42 -0400

    back to ifs

--- modals/remind-modal.js
+++ modals/remind-modal.js
@@ -2,55 +2,70 @@ const fetch = require("isomorphic-fetch");
 const chrono = require("chrono-node");
 
 const execute = async (interaction) => {
+    let response;
     const reminder = interaction.fields.getTextInputValue("remindercontent");
-    const delay = interaction.fields.getTextInputValue("delay");
-    const time = chrono.parseDate(interaction.fields.getTextInputValue("date"));
+    if (interaction.fields.getTextInputValue("date")) {
+        const time = chrono.parseDate(
+            interaction.fields.getTextInputValue("date")
+        );
 
-    const date = new Date(time);
+        const date = new Date(time);
 
-    if (date.toString() === "Invalid Date") {
-        interaction.reply({
-            content: "Error processing date.",
-            ephemeral: true,
-        });
-        return;
-    } else {
+        if (date.toString() === "Invalid Date") {
+            interaction.reply({
+                content: "Error processing date.",
+            });
+            return;
+        }
+
+        response = await fetch(
+            "https://qstash.upstash.io/v1/publish/https://hunter-bot-production.up.railway.app/reminders",
+            {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer " + process.env.QSTASH_TOKEN,
+                    "Content-type": "application/json",
+                    "Upstash-Not-Before": Math.floor(date.getTime() / 1000),
+                },
+                body: JSON.stringify({
+                    key: process.env.KEY,
+                    uid: interaction.user.id,
+                    content: reminder,
+                }),
+            }
+        );
+    } else if (interaction.fields.getTextInputValue("delay")) {
+        const delay = interaction.fields.getTextInputValue("delay");
+
         const regex = /^[0-9]+$/;
         if (!delay.match(regex)) {
             interaction.reply({
                 content: "Error processing delay.",
-                ephemeral: true,
             });
             return;
         }
+
+        response = await fetch(
+            "https://qstash.upstash.io/v1/publish/https://hunter-bot-production.up.railway.app/reminders",
+            {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer " + process.env.QSTASH_TOKEN,
+                    "Content-type": "application/json",
+                    "Upstash-Delay": `${delay}m`,
+                },
+                body: JSON.stringify({
+                    key: process.env.KEY,
+                    uid: interaction.user.id,
+                    content: reminder,
+                }),
+            }
+        );
+    } else {
+        await interaction.reply({
+            content: "Reminder must have a date/delay.",
+        });
     }
-
-    console.log({
-        "Upstash-Not-Before": time
-            ? Math.floor(date.getTime() / 1000)
-            : undefined,
-        "Upstash-Delay": delay,
-    });
-
-    const response = await fetch(
-        "https://qstash.upstash.io/v1/publish/https://hunter-bot-production.up.railway.app/reminders",
-        {
-            method: "POST",
-            headers: {
-                Authorization: "Bearer " + process.env.QSTASH_TOKEN,
-                "Content-type": "application/json",
-                "Upstash-Not-Before": time
-                    ? Math.floor(date.getTime() / 1000)
-                    : undefined,
-                "Upstash-Delay": `${delay}m`,
-            },
-            body: JSON.stringify({
-                key: process.env.KEY,
-                uid: interaction.user.id,
-                content: reminder,
-            }),
-        }
-    );
     if (!response.ok) {
         await interaction.reply({
             content: "Failed to set reminder, please try again later.",
