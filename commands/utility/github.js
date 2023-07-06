ae0b88e232361227c772fa26d3079be9b3304f5f
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2023-07-06 08:31:00 +0200

    minor spelling error :skull:

--- commands/utility/github.js
+++ commands/utility/github.js
@@ -27,28 +27,27 @@ const data = new SlashCommandBuilder()
     );
 
 const execute = async (interaction) => {
-        const discriminatorInput = new TextInputBuilder()
-            .setCustomId("discriminator")
-            .setLabel("Discriminator:")
-            .setPlaceholder("leave blank for random")
-            .setRequired(false)
-            .setStyle(TextInputStyle.Short);
-        const channelInput = new TextInputBuilder()
-            .setCustomId("channel")
-            .setLabel("Channel:")
-            .setPlaceholder(
-                "Channel ID of the channel to receive updates to (leave blank for DMs)"
-            )
-            .setRequired(false)
-            .setStyle(TextInputStyle.Short);
-        const row1 = new ActionRowBuilder().addComponents(discriminatorInput);
-        const row2 = new ActionRowBuilder().addComponents(channelInput);
-        const modal = new ModalBuilder()
-            .setCustomId("gh-register")
-            .setTitle("Endpoint Generator")
-            .addComponents(row1, row2);
-        await interaction.showModal(modal);
-    }
+    const discriminatorInput = new TextInputBuilder()
+        .setCustomId("discriminator")
+        .setLabel("Discriminator:")
+        .setPlaceholder("leave blank for random")
+        .setRequired(false)
+        .setStyle(TextInputStyle.Short);
+    const channelInput = new TextInputBuilder()
+        .setCustomId("channel")
+        .setLabel("Channel:")
+        .setPlaceholder(
+            "Channel ID of the channel to receive updates to (leave blank for DMs)"
+        )
+        .setRequired(false)
+        .setStyle(TextInputStyle.Short);
+    const row1 = new ActionRowBuilder().addComponents(discriminatorInput);
+    const row2 = new ActionRowBuilder().addComponents(channelInput);
+    const modal = new ModalBuilder()
+        .setCustomId("gh-register")
+        .setTitle("Endpoint Generator")
+        .addComponents(row1, row2);
+    await interaction.showModal(modal);
 };
 
 module.exports = {
