754ad9ce89003bc633d49b9a41b574c08fedc564
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2023-09-12 15:13:50 -0500

    a

--- rest-server.js
+++ rest-server.js
@@ -25,7 +25,7 @@ const start = async (client) => {
         //* -------------------------------------------------------------------------------------------- /message
         if (request.body.key !== process.env.MESSAGE_KEY) {
             console.log("Invalid key for /message.");
-            return { text: "Invalid key." };
+            return { status: "Invalid key." };
         }
         const { channelID, message } = request.body;
 
