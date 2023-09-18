3907f6ce0d94853cef945c50848de471c8fed11b
Cohen Smith <cohens@mail.smu.edu>
2023-09-18 17:25:36 -0500

    jarvisBot

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,6 +2,10 @@ const { Events } = require("discord.js");
 
 const name = Events.MessageCreate;
 
+const names = ["jarvis", "alexa"]
+const inputs = ["Jarvis send", "Jarvis hit us with", "Jarvis throw", "jarvis hit me with", "jarvis paste"]
+const words = ["the", "up the", "in the", "a", "up a", "in a"]
+
 const execute = async (message) => {
     if (message.content.toLowerCase() === "hello there") {
         try {
@@ -39,6 +43,31 @@ const execute = async (message) => {
                 }
             }
             await message.reply({ content: newMessage });
+        }
+    }
+    //jarvis bot
+    if (message.content.toLowerCase().startsWith("jarvis") || message.content.toLowerCase().startsWith("alexa")) {
+        const commas = ["",","];
+        for (x = 0; x < names.length * commas.length * inputs.length * words.length; x++) {
+            const name = names[Math.floor(x / (comma.length * inputs.length * words.length))];
+            const comma = commas[Math.floor((x % (comma.length * inputs.length * words.length)) / (inputs.length * words.length))];
+            const input = inputs[Math.floor((x % (inputs.length * words.length)) / words.length)];
+            const word = words[x % words.length];
+            if (message.content.toLowerCase().startsWith(name + comma + input + word)) {
+                const alias = message.content.toLowerCase().replace(name + comma + input + word, "");
+                const regex = /^[A-Za-z0-9\s-_,.]+$/;
+                if (!alias.match(regex) || alias.toLowerCase() === "null") {
+                    await message.reply({ content: "Which one u talkin bout?" });
+                    break;
+                }
+                const data = await prisma.gif.findFirst({ where: { uid: message.CLIENT_ID, alias } });
+                if (!data) {
+                    await message.reply({ content: "I aint got dat one on deck" });
+                    break;
+                }
+                await message.reply(data.link);
+                break;
+            }
         }
     }
 };
