1f06121206e709e2de81d948389673b367bc575b
Hunter-The-Great <66389989+Hunter-The-Great@users.noreply.github.com>
2024-02-21 10:26:18 -0600

    less spammy

--- events/ready.js
+++ events/ready.js
@@ -8,6 +8,7 @@ const execute = async (client) => {
     console.log(`Logged in as ${client.user.tag}`);
     var os = require("os");
     try {
+        if (os.hostname() === "MacBook-Pro.local") return;
         client.channels.fetch("1126759333733085214").then((channel) => {
             channel.send(
                 `# Hunter bot is live on ${
