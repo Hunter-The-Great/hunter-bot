3152fc927d7204108c6516eeee0db6efbce8a33e
Hunter-The-Great <benlowry9@gmail.com>
2024-04-22 23:28:11 -0500

    this is dumb

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -2,7 +2,7 @@
 import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import fs from "node:fs";
 import path from "node:path";
-
+console.log("e");
 const commands: SlashCommandBuilder[] = [];
 // Grab all the command files from the commands directory you created earlier
 const foldersPath = path.join(__dirname, "commands");
