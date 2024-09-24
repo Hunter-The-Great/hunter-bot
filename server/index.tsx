bb00dfc60c35fdefa117ef820f03c475e93341d1
Hunter-The-Great <benlowry9@gmail.com>
2024-09-24 13:09:43 -0500

    type stuff

--- server/index.tsx
+++ server/index.tsx
@@ -3,16 +3,35 @@ import websocket from "@fastify/websocket";
 import cors from "@fastify/cors";
 import { EmbedBuilder, TextChannel } from "discord.js";
 import { sentry } from "../utilities/sentry";
-import { FastifyReply, fastify } from "fastify";
+import { FastifyReply, FastifyRequest, fastify } from "fastify";
 import { BaseHtml } from "./baseHtml";
 import { FeedbackHtml } from "./feedback";
+import { z, ZodSchema } from "zod";
+import { FastifyRequestType } from "fastify/types/type-provider";
+import { log } from "console";
 
-export const server = fastify() as any;
+export const server = fastify();
 
 export function updateSite(message: JSX.Element) {
     server.websocketServer.clients.forEach((client) => {
         client.send(message.toString());
     });
+}
+
+function useSchema<T extends ZodSchema>(
+    schema: T,
+    handler: (
+        req: FastifyRequestType<unknown, unknown, unknown, z.infer<T>>,
+        res: FastifyReply
+    ) => void
+) {
+    return async (req: FastifyRequest, res: FastifyReply) => {
+        const result = schema.safeParse(req.body);
+        if (result.error) {
+            return res.code(400).send(result.error);
+        }
+        return handler(req, res);
+    };
 }
 
 const start = async (client) => {
@@ -27,7 +46,7 @@ const start = async (client) => {
     //   prefix: "/public/",
     // });
 
-    server.get("/", async (req, res: FastifyReply) => {
+    server.get("/", async (req, res) => {
         res.header("Content-Type", "text/html; charset=utf-8");
         res.send(
             <BaseHtml title="Hunter Bot">
@@ -55,207 +74,280 @@ const start = async (client) => {
         );
     });
 
-    server.get("/feedback", async (req, res: FastifyReply) => {
+    server.get("/feedback", async (req, res) => {
         res.header("Content-Type", "text/html; charset=utf-8");
         res.send(<BaseHtml>{await (<FeedbackHtml />)}</BaseHtml>);
     });
 
     server.get("/pubsub", { websocket: true }, (_connection) => {});
 
-    server.post("/reminders", async (request, res) => {
-        if (request.body.key !== process.env.KEY) {
-            console.log("Invalid key for /reminders.");
-            return res.code(401).send({ message: "Invalid Key" });
-        }
-
-        const { uid, content } = request.body;
-        const user = await client.users.fetch(uid);
-        await user.send(content);
-        return res.code(200).send({ message: "Acknowledged." });
+    const reminderSchema = z.object({
+        key: z.string(),
+        uid: z.string(),
+        content: z.string(),
     });
 
-    server.post("/message", async (request, res) => {
-        const message = request.body.message;
-        const channelID = request.body.channel;
-        const key = request.body.key;
+    server.post(
+        "/reminders",
+        useSchema(reminderSchema, async (req, res) => {
+            const { key, uid, content } = req.body;
 
-        if (key !== process.env.MESSAGE_KEY) {
-            return res.code(401).send({ message: "Unauthorized" });
-        }
+            if (key !== process.env.KEY) {
+                console.log("Invalid key for /reminders.");
+                return res.code(401).send({ message: "Invalid Key" });
+            }
 
-        const channel = await client.channels.fetch(channelID);
+            const user = await client.users.fetch(uid);
+            await user.send(content);
+            return res.code(200).send({ message: "Acknowledged." });
+        })
+    );
 
-        await channel.send(message);
-        return res.send(
-            <div id="message" class="flex justify-center items-end">
-                <textarea
-                    name="message"
-                    class="resize max-h-48 max-w-lg min-w-48 min-h-16 rounded mt-1 mr-1 bg-slate-950 p-1"
-                ></textarea>
-                <button
-                    type="submit"
-                    class="bg-sky-500 hover:bg-sky-700 rounded p-1 max-h-10 min-w-20"
-                >
-                    Send
-                </button>
-            </div>
-        );
+    const messageSchema = z.object({
+        key: z.string(),
+        channel: z.string(),
+        message: z.string(),
     });
 
-    server.post("/gh/:uid/:discriminator", async (request, res) => {
-        const { uid, discriminator } = request.params;
-        const user = await client.users.fetch(uid);
+    server.post(
+        "/message",
+        useSchema(messageSchema, async (req, res) => {
+            const { key, message, channel } = req.body;
 
-        const webhook = await prisma.gitHubWebhook.findFirst({
-            where: { uid, discriminator },
-        });
-        if (!webhook) {
-            return res.code(404).send({ message: "Endpoint not found." });
-        }
-        const embed = new EmbedBuilder()
-            .setColor(0x00ffff)
-            .setTitle(
-                request.body.repository.full_name ||
-                    "Could not find repository information."
-            )
-            .setDescription(
-                request.body.head_commit.message ||
-                    "Could not find commit message."
-            )
-            .addFields([
-                {
-                    name: "Commit",
-                    value:
-                        `[${request.body.head_commit.id.slice(0, 7)}](${
-                            request.body.head_commit.url
-                        })` || "Could not find commit information.",
-                    inline: true,
-                },
-                {
-                    name: "Author",
-                    value:
-                        `[${request.body.sender.login}](${request.body.sender.url})` ||
-                        "Could not find author information.",
-                    inline: true,
-                },
-                {
-                    name: "Branch",
-                    value:
-                        request.body.ref ||
-                        "Could not find branch information.",
-                    inline: true,
-                },
-                {
-                    name: "Timestamp",
-                    value:
-                        request.body.head_commit.timestamp ||
-                        "Could not find timestamp.",
-                    inline: true,
-                },
-            ]);
-        if (webhook.channelID === "-1") {
-            await user.send({ embeds: [embed] });
-        } else {
-            const channel = await client.channels.fetch(webhook.channelID);
-            await channel.send({ embeds: [embed] });
-        }
-        return res.code(200).send({ message: "Acknowledged." });
-    });
+            if (key !== process.env.MESSAGE_KEY) {
+                return res.code(401).send({ message: "Unauthorized" });
+            }
 
-    server.post("/drewh", async (request, res) => {
-        if (request.body.key !== process.env.DREW_KEY) {
-            console.log("Invalid key for /drewh.");
-            return res.code(401).send({ message: "Invalid Key" });
-        }
-        const drew = await client.users.fetch("254591447284711424");
-        await drew.send(request.body.message);
-        return res.code(200).send({ message: "Acknowledged." });
-    });
+            const channelobj = await client.channels.fetch(channel);
 
-    server.post("/coolthing", async (request, res) => {
-        await client.channels.fetch("1198755163612119163").then((channel) => {
-            channel.send(
-                `${request.body.user ?? "SOMEONE"} ${
-                    request.body.code ? `(${request.body.code}) ` : ""
-                }FOUND THE COOL THING!`
-            );
-        });
-        return res.code(200).send({ message: "Cool thing acknowledged." });
-    });
-
-    server.post("/login", async (req, res: FastifyReply) => {
-        res.header("Content-Type", "text/html; charset=utf-8");
-        if (req.body.key !== process.env.MESSAGE_KEY) {
-            res.code(401).send(
-                <div class="flex text-red-700 justify-center">Invalid Key</div>
-            );
-            return;
-        }
-        res.send(
-            <form
-                hx-post="/message"
-                hx-ext="json-enc"
-                class="flex-col justify-center scale-150"
-                hx-swap="outerHTML"
-                hx-target="#message"
-            >
-                <div id="key">
-                    <input
-                        type="hidden"
-                        name="key"
-                        value={process.env.MESSAGE_KEY}
-                    />
-                </div>
-                <div id="guilds">
-                    <label>Guild: </label>
-                    <select
-                        name="guild"
-                        hx-swap="outerHTML"
-                        hx-target="#channels"
-                        hx-post="/guild"
-                        hx-ext="json-enc"
-                        class="rounded bg-slate-950"
+            await channelobj.send(message);
+            return res.send(
+                <div id="message" class="flex justify-center items-end">
+                    <textarea
+                        name="message"
+                        class="resize max-h-48 max-w-lg min-w-48 min-h-16 rounded mt-1 mr-1 bg-slate-950 p-1"
+                    ></textarea>
+                    <button
+                        type="submit"
+                        class="bg-sky-500 hover:bg-sky-700 rounded p-1 max-h-10 min-w-20"
                     >
-                        <option>Select a guild</option>
-                        {client.guilds.cache.map((guild) => (
-                            <option value={guild.id}>{guild.name}</option>
-                        ))}
-                    </select>
+                        Send
+                    </button>
                 </div>
-                <div id="channels"></div>
-                <div id="message"></div>
-            </form>
-        );
+            );
+        })
+    );
+
+    const ghSchema = z.object({
+        repository: z.object({
+            full_name: z.string(),
+        }),
+        head_commit: z.object({
+            message: z.string(),
+            id: z.string(),
+            url: z.string(),
+            timestamp: z.string(),
+        }),
+        sender: z.object({
+            login: z.string(),
+            url: z.string(),
+        }),
+        ref: z.string(),
     });
 
-    server.post("/guild", async (req, res: FastifyReply) => {
-        res.header("Content-Type", "text/html; charset=utf-8");
-        res.send(
-            <div id="channels">
-                <label>Channel: </label>
-                <select
-                    name="channel"
+    server.post(
+        "/gh/:uid/:discriminator",
+        useSchema(ghSchema, async (req, res) => {
+            const { uid, discriminator } = req.params;
+            const user = await client.users.fetch(uid);
+
+            const webhook = await prisma.gitHubWebhook.findFirst({
+                where: { uid, discriminator },
+            });
+            if (!webhook) {
+                return res.code(404).send({ message: "Endpoint not found." });
+            }
+            const embed = new EmbedBuilder()
+                .setColor(0x00ffff)
+                .setTitle(
+                    req.body.repository.full_name ||
+                        "Could not find repository information."
+                )
+                .setDescription(
+                    req.body.head_commit.message ||
+                        "Could not find commit message."
+                )
+                .addFields([
+                    {
+                        name: "Commit",
+                        value:
+                            `[${req.body.head_commit.id.slice(0, 7)}](${
+                                req.body.head_commit.url
+                            })` || "Could not find commit information.",
+                        inline: true,
+                    },
+                    {
+                        name: "Author",
+                        value:
+                            `[${req.body.sender.login}](${req.body.sender.url})` ||
+                            "Could not find author information.",
+                        inline: true,
+                    },
+                    {
+                        name: "Branch",
+                        value:
+                            req.body.ref ||
+                            "Could not find branch information.",
+                        inline: true,
+                    },
+                    {
+                        name: "Timestamp",
+                        value:
+                            req.body.head_commit.timestamp ||
+                            "Could not find timestamp.",
+                        inline: true,
+                    },
+                ]);
+            if (webhook.channelID === "-1") {
+                await user.send({ embeds: [embed] });
+            } else {
+                const channel = await client.channels.fetch(webhook.channelID);
+                await channel.send({ embeds: [embed] });
+            }
+            return res.code(200).send({ message: "Acknowledged." });
+        })
+    );
+
+    const drewhSchema = z.object({
+        key: z.string(),
+        message: z.string(),
+    });
+
+    server.post(
+        "/drewh",
+        useSchema(drewhSchema, async (req, res) => {
+            if (req.body.key !== process.env.DREW_KEY) {
+                console.log("Invalid key for /drewh.");
+                return res.code(401).send({ message: "Invalid Key" });
+            }
+            const drew = await client.users.fetch("254591447284711424");
+            await drew.send(req.body.message);
+            return res.code(200).send({ message: "Acknowledged." });
+        })
+    );
+
+    const coolthingSchema = z.object({
+        user: z.string().optional(),
+        code: z.string().optional(),
+    });
+
+    server.post(
+        "/coolthing",
+        useSchema(coolthingSchema, async (req, res) => {
+            await client.channels
+                .fetch("1198755163612119163")
+                .then((channel) => {
+                    channel.send(
+                        `${req.body.user ?? "SOMEONE"} ${
+                            req.body.code ?? ""
+                        }FOUND THE COOL THING!`
+                    );
+                });
+            return res.code(200).send({ message: "Cool thing acknowledged." });
+        })
+    );
+
+    const loginSchema = z.object({
+        key: z.string(),
+    });
+
+    server.post(
+        "/login",
+        useSchema(loginSchema, async (req, res) => {
+            res.header("Content-Type", "text/html; charset=utf-8");
+            if (req.body.key !== process.env.MESSAGE_KEY) {
+                res.code(401).send(
+                    <div class="flex text-red-700 justify-center">
+                        Invalid Key
+                    </div>
+                );
+                return;
+            }
+            res.send(
+                <form
+                    hx-post="/message"
                     hx-ext="json-enc"
+                    class="flex-col justify-center scale-150"
                     hx-swap="outerHTML"
                     hx-target="#message"
-                    hx-post="/channel"
-                    class="rounded bg-slate-950"
                 >
-                    <option>Select a channel</option>
-                    {client.guilds.cache
-                        .get(req.body.guild)
-                        .channels.cache.filter(
-                            (channel) => channel instanceof TextChannel
-                        )
-                        .map((channel) => (
-                            <option value={channel.id}>{channel.name}</option>
-                        ))}
-                </select>
-            </div>
-        );
+                    <div id="key">
+                        <input
+                            type="hidden"
+                            name="key"
+                            value={process.env.MESSAGE_KEY}
+                        />
+                    </div>
+                    <div id="guilds">
+                        <label>Guild: </label>
+                        <select
+                            name="guild"
+                            hx-swap="outerHTML"
+                            hx-target="#channels"
+                            hx-post="/guild"
+                            hx-ext="json-enc"
+                            class="rounded bg-slate-950"
+                        >
+                            <option>Select a guild</option>
+                            {client.guilds.cache.map((guild) => (
+                                <option value={guild.id}>{guild.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div id="channels"></div>
+                    <div id="message"></div>
+                </form>
+            );
+        })
+    );
+
+    const guildSchema = z.object({
+        guild: z.string(),
     });
 
-    server.post("/channel", async (req, res: FastifyReply) => {
+    server.post(
+        "/guild",
+        useSchema(guildSchema, async (req, res) => {
+            res.header("Content-Type", "text/html; charset=utf-8");
+            res.send(
+                <div id="channels">
+                    <label>Channel: </label>
+                    <select
+                        name="channel"
+                        hx-ext="json-enc"
+                        hx-swap="outerHTML"
+                        hx-target="#message"
+                        hx-post="/channel"
+                        class="rounded bg-slate-950"
+                    >
+                        <option>Select a channel</option>
+                        {client.guilds.cache
+                            .get(req.body.guild)
+                            .channels.cache.filter(
+                                (channel) => channel instanceof TextChannel
+                            )
+                            .map((channel) => (
+                                <option value={channel.id}>
+                                    {channel.name}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+            );
+        })
+    );
+
+    server.post("/channel", async (req, res) => {
         res.header("Content-Type", "text/html; charset=utf-8");
         res.send(
             <div id="message" class="flex justify-center items-end">
@@ -273,7 +365,7 @@ const start = async (client) => {
         );
     });
 
-    server.delete("/feedback-remove/:id", async (req, res: FastifyReply) => {
+    server.delete("/feedback-remove/:id", async (req, res) => {
         const id = req.params.id;
         await prisma.feedback.delete({
             where: { id },
@@ -283,7 +375,7 @@ const start = async (client) => {
 
     //* running the server
     try {
-        server.listen({ host: "0.0.0.0", port: process.env.PORT });
+        server.listen({ host: "0.0.0.0", port: parseInt(process.env.PORT!) });
         console.log("Listening on: " + process.env.PORT);
     } catch (err) {
         sentry.captureException(err);
