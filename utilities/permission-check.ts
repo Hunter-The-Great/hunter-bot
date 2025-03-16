56a3535aa3b82392895990dfa3be4b98716088e1
Hunter-The-Great <benlowry9@gmail.com>
2025-03-16 00:57:37 -0500

    permissions check fix

--- utilities/permission-check.ts
+++ utilities/permission-check.ts
@@ -1,15 +1,21 @@
 import { PermissionsBitField } from "discord.js";
 
 const checkPermissions = async (uid, channel) => {
-    const member = await channel.guild.members.fetch({
-        force: true,
-        user: uid,
-    });
+    try {
+        if (!channel.guild) return false;
 
-    return (
-        member.permissions.has(PermissionsBitField.Flags.Administrator) ||
-        member === channel.guild.ownerId
-    );
+        const member = await channel.guild.members.fetch({
+            force: true,
+            user: uid,
+        });
+
+        return (
+            member.permissions.has(PermissionsBitField.Flags.Administrator) ||
+            member === channel.guild.ownerId
+        );
+    } catch (error) {
+        return false;
+    }
 };
 
 export { checkPermissions };
